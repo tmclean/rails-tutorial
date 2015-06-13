e42dee5515085d0e6aea7c538541106d4f8e2d96
Tom McLean <tjrage19@gmail.com>
2015-06-13 22:21:06 +0000

    Improved email regext and email testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     
     validates :name,  presence: true, length: { maximum: 50  }
     
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
                       
     validates :password, presence: true, length: { minimum: 6 }
                       
-    before_save { self.email = email.downcase }
+    before_save { email.downcase! }
     
     has_secure_password
 end
