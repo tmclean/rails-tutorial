053b44553cfe20c387c309a44881bf9429c030da
Tom McLean <tjrage19@gmail.com>
2015-06-14 23:34:08 +0000

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
                       
-    validates :password, presence: true, length: { minimum: 6 }
+    validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
                       
     before_save { email.downcase! }
     
