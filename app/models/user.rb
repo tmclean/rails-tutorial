1f8cda48d789966192d9011722a49322ad254b3e
Tom McLean <tjrage19@gmail.com>
2015-06-15 02:49:54 +0000

    Added password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
     
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     
     validates :name,  presence: true, length: { maximum: 50  }
     
@@ -43,6 +43,16 @@ class User < ActiveRecord::Base
         UserMailer.account_activation( self ).deliver_now
     end
     
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute( :reset_digest, User.digest( reset_token ) )
+        update_attribute( :reset_sent_at, Time.zone.now )
+    end
+    
+    def send_password_reset_email
+        UserMailer.password_reset( self ).deliver_now
+    end
+    
     def User.digest( string )
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
         BCrypt::Password.create( string, cost: cost )
@@ -59,5 +69,9 @@ class User < ActiveRecord::Base
     def create_activation_digest
         self.activation_token = User.new_token
         self.activation_digest = User.digest( activation_token )
+    end
+    
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
     end
 end
