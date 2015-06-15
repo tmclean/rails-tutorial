46cb0bece682449966cd361c6c5039685957a7a0
Tom McLean <tjrage19@gmail.com>
2015-06-15 20:49:05 +0000

    Fixing mailer domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'smtp.mailgun.org'
+  host = 'tmclean-tutorial-sample-app.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address              => 'smtp.mailgun.org',
@@ -72,7 +72,7 @@ Rails.application.configure do
     :authentication       => :plain,
     :user_name            => ENV['SMTP_USERNAME'],
     :password             => ENV['SMTP_PASSWORD'],
-    :domain               => 'tmclean-tutorial-sample-app.herokuapp.com',
+    :domain               => 'heroku.com',
     :enable_starttls_auto => true
   }
   
